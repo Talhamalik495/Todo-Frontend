8386bdd748653b9cb2b1dcafba4976d631f9b4aa
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2025-01-11 19:10:28 +0500

    update todo frontend

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,6 @@ function Header() {
 
   return (
     <header className="bg-blue-600 text-white p-4 flex justify-between items-center">
-      {/* لوگو */}
       <div className="flex items-center">
         <img
           src="https://via.placeholder.com/40"
@@ -21,32 +20,30 @@ function Header() {
       {/* نیویگیشن بٹن */}
       <nav>
         <ul className="flex space-x-4">
-          {!user.email ? (
+          <li>
+            <button
+              className="bg-white text-blue-600 px-4 py-2 rounded hover:bg-gray-200"
+              onClick={() => {
+                setUser("");
+                Cookies.set(null);
+              }}
+            >
+              Logout
+            </button>
+          </li>
+
+          <div>
             <li>
-              <button
-                className="bg-white text-blue-600 px-4 py-2 rounded hover:bg-gray-200"
-                onClick={() => {
-                  setUser("");
-                  Cookies.set(null);
-                }}
-              >
-                Logout
+              <button className="bg-white text-blue-600 px-4 py-2 rounded hover:bg-gray-200">
+                <Link to={"/singin"}>Login</Link>
               </button>
             </li>
-          ) : (
-            <div>
-              <li>
-                <button className="bg-white text-blue-600 px-4 py-2 rounded hover:bg-gray-200">
-                  <Link to={"/singin"}>Login</Link>
-                </button>
-              </li>
-              <li>
-                <button className="bg-white text-blue-600 px-4 py-2 rounded hover:bg-gray-200">
-                  <Link to={"/singup"}>Signup</Link>
-                </button>
-              </li>
-            </div>
-          )}
+            <li>
+              <button className="bg-white text-blue-600 px-4 py-2 rounded hover:bg-gray-200">
+                <Link to={"/singup"}>Signup</Link>
+              </button>
+            </li>
+          </div>
         </ul>
       </nav>
     </header>
